e0cc2515ba68ec484957179d6cfba5f6c7050fb3
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2023-02-08 14:25:50 +0100

    Updated icons

--- app/components/BookingTabs.tsx
+++ app/components/BookingTabs.tsx
@@ -12,9 +12,9 @@ const ListRoute = () => <BookingList />
 export const BookingTabs = observer(function BookingTabs() {
   const [index, setIndex] = React.useState(0)
   const [routes] = React.useState([
-    { key: "charts", title: "Charts", focusedIcon: "album" },
-    { key: "calendar", title: "Calendar", focusedIcon: "heart", unfocusedIcon: "heart-outline" },
-    { key: "list", title: "List", focusedIcon: "history" },
+    { key: "charts", title: "Charts", focusedIcon: "chart-donut" },
+    { key: "calendar", title: "Calendar", focusedIcon: "calendar-check", unfocusedIcon: "calendar-check-outline" },
+    { key: "list", title: "List", focusedIcon: "view-list", unfocusedIcon: "view-list-outline" },
   ])
 
   const renderScene = BottomNavigation.SceneMap({
