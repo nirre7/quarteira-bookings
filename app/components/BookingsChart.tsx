d75bc4878e0635149fe7cabff2387a7283b0ff71
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2023-02-09 08:30:20 +0100

    Updated charts

--- app/components/BookingsChart.tsx
+++ app/components/BookingsChart.tsx
@@ -1,36 +1,128 @@
 import * as React from "react"
-import { Dimensions } from "react-native"
+import { Dimensions, ScrollView, View, ViewStyle } from "react-native"
 import { observer } from "mobx-react-lite"
-import { ProgressChart } from "react-native-chart-kit"
-import { ChartConfig } from "react-native-chart-kit/dist/HelperTypes"
+import { VictoryLabel, VictoryPie } from "victory-native"
+import Svg from "react-native-svg"
+import { VictoryLabelStyleObject } from "victory-core/src/victory-theme/types"
+import { Text } from "react-native-paper"
+import { useStores } from "../models"
+import { differenceInCalendarDays, eachDayOfInterval, getDaysInMonth, getMonth } from "date-fns"
+import { translate, TxKeyPath } from "../i18n"
 
-/**
- * Bookings chart(s)
- */
-export const BookingsChart = observer(function BookingsChart() {
+function getProgressChart(width: number, datesBooked: number, titleLabel: TxKeyPath) {
 
-  const chartData = {
-    labels: ["June", "July", "August"],
-    data: [0.4, 0.6, 0.8],
+  return (
+    <View style={{ width: width }}>
+      <Text variant={"titleSmall"}
+            style={title}>
+        {translate(titleLabel)}
+      </Text>
+      <Svg viewBox="0 0 400 400"
+           width="100%"
+           height="100%">
+        <VictoryPie
+          standalone={false}
+          animate={{ duration: 1000 }}
+          data={[{ x: 1, y: datesBooked }, { x: 2, y: 100 - datesBooked }]}
+          innerRadius={120}
+          cornerRadius={25}
+          labels={() => null}
+          style={{
+            data: {
+              fill: ({ datum }) => {
+                return datum.x === 1 ? "green" : "transparent"
+              },
+            },
+          }}
+        />
+        <VictoryLabel
+          textAnchor="middle"
+          verticalAnchor="middle"
+          x={200}
+          y={200}
+          text={`${Math.round(datesBooked)}%`}
+          style={pieChartLabel}
+        />
+      </Svg>
+    </View>
+  )
+}
+
+
+function getMonthCharts(aThirdOfTheScreenSize: number, numberOfBookedDaysPerMonth: Map<number, number>) {
+
+  const chunkSize = 3
+  const quarters = []
+
+  for (let i = 0; i < 12; i += chunkSize) {
+    const quarter = Array.from(numberOfBookedDaysPerMonth.entries()).slice(i, i + chunkSize)
+    quarters.push(quarter)
   }
 
-  const chartConfig: ChartConfig = {
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
-    barPercentage: 0.5,
-    useShadowColorFromDataset: false,
-    backgroundColor: "#fff",
+  return quarters.map(quarter => {
+
+    return (
+      <View style={{ flexDirection: "row" }}>
+        {quarter.map(month => {
+          return getProgressChart(aThirdOfTheScreenSize, month[1], `charts.month_${month[0]}` as TxKeyPath)
+        })}
+      </View>
+    )
+  })
+}
+
+export const BookingsChart = observer(function BookingsChart() {
+  const { bookingStore } = useStores()
+
+  const halfScreenSize = Dimensions.get("screen").width / 2
+  const aThirdOfTheScreenSize = Dimensions.get("screen").width / 3
+
+  const today = new Date()
+  const bookings = bookingStore.activeBookings
+  const allBookingDates = bookings.flatMap(b => eachDayOfInterval({ start: b.start, end: b.end }))
+  const datesDuringHighSeason = allBookingDates.filter(b => getMonth(b) === 5 || getMonth(b) === 6 || getMonth(b) === 7)
+  const datesDuringLowSeason = allBookingDates.filter(b => getMonth(b) !== 5 && getMonth(b) !== 6 && getMonth(b) !== 7)
+
+  const numberOfDaysForThisYear = differenceInCalendarDays(new Date(today.getFullYear(), 11, 31), new Date(today.getFullYear(), 0, 1))
+  const numberOfDaysDuringHighSeason = differenceInCalendarDays(new Date(today.getFullYear(), 7, 31), new Date(today.getFullYear(), 4, 1))
+  const numberOfDaysDuringLowSeason = numberOfDaysForThisYear - numberOfDaysDuringHighSeason
+  const datesBookedDuringHighSeason = (datesDuringHighSeason.length / numberOfDaysDuringHighSeason) * 100
+  const datesBookedDuringLowSeason = (datesDuringLowSeason.length / numberOfDaysDuringLowSeason) * 100
+
+  const numberOfBookedDaysPerMonth = new Map<number, number>()
+  for (let i = 0; i < 12; i++) {
+    const bookedDaysPerMonth = allBookingDates.filter(b => getMonth(b) === i).length
+    const daysInMonth = getDaysInMonth(new Date(today.getFullYear(), i))
+    numberOfBookedDaysPerMonth.set(i, (bookedDaysPerMonth / daysInMonth) * 100)
   }
 
   return (
-    <ProgressChart
-      data={chartData}
-      width={Dimensions.get("screen").width}
-      height={250}
-      strokeWidth={12}
-      chartConfig={chartConfig}
-      radius={30}
-      hideLegend={false}
-    />
+    <ScrollView>
+      <View style={wrapper}>
+        {getProgressChart(halfScreenSize, datesBookedDuringHighSeason, "charts.highSeasonTitle")}
+        {getProgressChart(halfScreenSize, datesBookedDuringLowSeason, "charts.lowSeasonTitle")}
+      </View>
+      <View style={wrapper2}>
+        {getMonthCharts(aThirdOfTheScreenSize, numberOfBookedDaysPerMonth)}
+      </View>
+    </ScrollView>
   )
 })
+
+const pieChartLabel: VictoryLabelStyleObject = {
+  fontSize: 50,
+}
+
+const wrapper: ViewStyle = {
+  flexDirection: "row",
+  height: 150,
+}
+
+const wrapper2: ViewStyle = {
+  height: 100,
+  paddingBottom: 5,
+}
+
+const title: ViewStyle = {
+  alignSelf: "center",
+}
